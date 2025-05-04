b4f70868dfb2e4fc6dac8f8d753bfe0a3ba04f1d
Unegbu Kingsley <kingsobino@gmail.com>
2025-05-04 12:27:20 +0100

    add authentication. verified signin and sign up

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  allow_unauthenticated_access only: %i[ create ]
+  rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_url, alert: "Try again later." }
+
+  def create
+    if user = User.authenticate_by(params.permit(:email_address, :password))
+      start_new_session_for user
+      redirect_to after_authentication_url
+    else
+      render json: { error: "Email or password is incorrect." }, status: :unauthorized 
+    end
+  end
+
+  def destroy
+    terminate_session
+    redirect_to new_session_path
+  end
+end
